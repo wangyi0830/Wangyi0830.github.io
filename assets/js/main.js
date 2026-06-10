bc5605a1f07b481c3433b51c743eac8b6d67ae6c
wangyi0830 <yolowyi@163.com>
2026-06-10 21:50:36 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -146,7 +146,7 @@
     {
       category: 'skills.AI Applications',
       items: [
-        { name: 'Vibe Coding', icon: 'fab fa-robot' },
+        { name: 'Vibe Coding', icon: 'fab fa-globe' },
         { name: '即梦', icon: 'fab fa-git-alt' },
         { name: '可灵', icon: 'fas fa-video' },
         { name: '海螺', icon: 'fas fa-water' },
